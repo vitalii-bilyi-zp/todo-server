5c9bd71f19138753f531f327ee4181e82a77b551
belyy_vv <belyy_vv@groupbwt.com>
2022-09-22 13:35:52 +0300

    feat: environment variables support

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,20 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProjectsModule } from './projects/projects.module';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/test'), ProjectsModule, TasksModule],
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                uri: configService.get<string>('MONGODB_URL'),
+            }),
+        }),
+        ProjectsModule,
+        TasksModule,
+    ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const configService = app.get(ConfigService);
+    app.enableCors({ origin: configService.get<string>('FRONTEND_URL') });
     app.useGlobalPipes(new ValidationPipe());
+
     await app.listen(3000);
 }
 bootstrap();
